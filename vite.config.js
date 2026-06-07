d49d0cb5a550ae2e5979dec0debacc404946452f
Jyotirmaya01 <sabatanant883@gmail.com>
2026-06-07 15:19:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     // This raises the warning limit to 1000 kBs
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 5000,
   },
 });
